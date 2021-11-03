1e2d1079dd477de1f4ddb54250e42b1292960de2
Ricardo Serrano <ricardo.ss@live.com>
2021-11-03 16:50:19 -0600

    exercise6(extra): Validate with useState

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,20 +6,34 @@ import * as React from 'react'
 function UsernameForm({onSubmitUsername}) {
 
   const inputUsername = React.useRef(null);
+  const [error, setError] = React.useState(null);
+  const [disabled, setDisabled] = React.useState(false);
 
   function handleSubmit(event){
     event.preventDefault();
     const username = inputUsername.current.value
     onSubmitUsername(username);
   };
+
+  function handleChange(event){
+    const currentValue = event.target.value;
+    const isValid = currentValue === currentValue.toLowerCase();
+    setError(isValid ? null : 'Username must be lower case');
+    setDisabled(isValid ? false : true);
+  }
  
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='usernameInput'>Username:</label>
-        <input ref={inputUsername} id='usernameInput' type="text" />
+        <input 
+          ref={inputUsername} 
+          id='usernameInput' 
+          type="text"
+          onChange={handleChange}/>
       </div>
-      <button type="submit">Submit</button>
+      <div role='alert'>{error}</div>
+      <button type="submit" disabled={disabled}>Submit</button>
     </form>
   )
 }
